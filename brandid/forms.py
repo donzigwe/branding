3e740240a7457984d4d49192f07757b4e53a4ac5
donzigwe <uzoigweiroh@gmail.com>
2016-03-31 11:26:15 +0100

    Changes in forms

--- brandid/forms.py
+++ brandid/forms.py
@@ -4,9 +4,7 @@ from brandid.models import *
 class TalkForm(forms.ModelForm):
     name =          forms.CharField()
     email =         forms.EmailField()
-    phone_number =  forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
-                    error_message = (" No Wrong format, please enter your phone number correctly"),
-                    )
+    phone_number =  forms.RegexField(regex=r'^\+?1?\d{9,15}$')
         
     class Meta:
         model = Talk
@@ -16,10 +14,8 @@ class TalkForm(forms.ModelForm):
 class ContactForm(forms.ModelForm):
     name =          forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Full Name'}))
     email =         forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    phone_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
-                                 error_message = ("Wrong format, please enter your phone number correctly"),
-                    required=False, widget=forms.TextInput(attrs={'placeholder': 'Search'}),
-                    )
+    phone_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$',
+                    required=False, widget=forms.TextInput(attrs={'placeholder': 'Search'}))
     #phone_number =  forms.CharField()
     description =       forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Tell Us what you want'}))
     
